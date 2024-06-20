7d4ca9799b0f7620c856d4ad560295df5f5c0d8e
fummyfummy <fuminaga115@g.ecc.u-tokyo.ac.jp>
2024-06-20 18:18:22 +0900

    Add files via upload

---
+++ manage_output.py
@@ -0,0 +1,6 @@
+
+def output(sentence_arrays, result):
+    resultsentence = []
+    for i in range(len(sentence_arrays)):
+        resultsentence.append([sentence_arrays[i], result[i]])
+    print(resultsentence)
