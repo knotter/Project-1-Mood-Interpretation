f87026808c66a34f799900aad22031094349ffde
Boyang CHEN <137773716+CHENBoyangutokyo@users.noreply.github.com>
2024-06-23 20:25:10 +0900

    Update load_input_data.py

--- my_nlp_project/src/my_library/load_input_data.py
+++ my_nlp_project/src/my_library/load_input_data.py
@@ -1,2 +1,5 @@
-def load(a):
-    return "Okay"
+def load(file_name):
+    with open(file_name, "r", encoding="utf-8") as file:
+        data = file.read()
+        splited_data = data.split()
+    return splited_data
