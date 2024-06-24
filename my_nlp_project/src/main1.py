d5b119fc84b0f3a66cf068863c10fb2fa730b75c
knotter <83846898+knotter@users.noreply.github.com>
2024-06-24 14:13:44 +0900

    Add files via upload

--- my_nlp_project/src/main1.py
+++ my_nlp_project/src/main1.py
@@ -10,20 +10,32 @@ import my_library.count_statistics as counter
 import my_library.predict_polarity as predictor
 import my_library.manage_output as output_manager
 
-sentence_arrays = input_loader.load("../data/processed_data_v1.txt")
-sentence = "友達と喧嘩してしまって、悲しい気持ちになった。"
+sentence_arrays = ["雨が降ってるから気分が落ち込む。早く晴れてほしいな。",
+"友達と映画を見に行ったけど、感動して泣いちゃった。",
+"新しい仕事が始まって緊張するけど、頑張りたい。",
+"美味しいケーキを食べて、幸せな気持ちになった。",
+"ペットが病気になって心配でたまらない。早く元気になって。",
+"家族と一緒に過ごす時間がとても楽しかった。",
+"仕事でミスをして上司に叱られてしまった。反省しないと。",
+"旅行の計画を立てているときが一番ワクワクする。",
+"久しぶりに会った友達と話して、元気が出た。",
+"電車が遅れてイライラするけど、仕方がない。"]
+
+#sentence = "コロナのせいで病院送りになって絶望していたが、治療後人並ならぬ賢さを手に入れた。"
 d1 = dictionary_loader.load_dictionary1("../ext-lib/dictionary1.txt")
 d2 = dictionary_loader.load_dictionary2("../ext-lib/dictionary2.txt")
 
+#print(sentence_arrays)
 #print(counter.regroup(d2).get("ほめ"))
 #print(d2.get(sentence[0:4]))
 #print(counter.count(d1, d2, sentence))
-print(predictor.predict_2(counter.count(d1, d2, sentence)))
+#print(predictor.predict_2(counter.count(d1, d2, sentence)))
 
 result = []
 for i in range(len(sentence_arrays)):
     sentence = sentence_arrays[i]
     count_statistics = counter.count(d1, d2, sentence)
-    result.append(predictor.predict(count_statistics))
+    result.append(predictor.predict_2(count_statistics))
 
-output_manager.output(sentence_arrays, result)
+print(result)
+#output_manager.output(sentence_arrays, result)
